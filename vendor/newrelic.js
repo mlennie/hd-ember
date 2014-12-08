adedd20ef91c40e829f2be154fed3fac227f601e
Monty Lennie <montylennie@gmail.com>
2014-12-08 17:06:21 +0100

    import new relic to brocfile

---
+++ vendor/newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['hd-ember'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '4f96767aa9b235c7ce46da7a3c386c2359142b03',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
