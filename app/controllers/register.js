49294da321c490b53b33bb74c486bb67ece00f60
Monty Lennie <montylennie@gmail.com>
2014-12-16 17:48:33 +0100

    add user registration logic

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -1,5 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  
+  email: '',
+  password: '',
+  passwordConfirmation: '',
+  user: function(){
+    return this.store.createRecord('user', {});
+  }.property(),
+  actions: {
+    registerUser: function() {
+      var user = this.get('user');
+      user.set('email', this.get('email'));
+      user.set('password', this.get('password'));
+      user.set('passwordConfirmation', this.get('passwordConfirmation'));
+      user.save().then(function() {
+        console.log(user);
+      });
+    }
+  }
 });
