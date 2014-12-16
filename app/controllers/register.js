9a62134385a4e6ad35f4f5c05316affd1c1892a3
Monty Lennie <montylennie@gmail.com>
2014-12-16 20:15:55 +0100

    finish basic registration functionality

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  registrationSuccessful: false,
+  registrationFailed: false,
+  response: '',
   email: '',
   password: '',
   passwordConfirmation: '',
@@ -9,13 +12,24 @@ export default Ember.Controller.extend({
   }.property(),
   actions: {
     registerUser: function() {
+      //set user
       var user = this.get('user');
       user.set('email', this.get('email'));
       user.set('password', this.get('password'));
       user.set('passwordConfirmation', this.get('passwordConfirmation'));
-      user.save().then(function() {
-        console.log(user);
-      });
+
+      //save user
+      var _this = this;
+      var onSuccess = function(user){
+        _this.set('registrationFailed', false);
+        _this.set('registrationSuccessful', true);
+      };
+
+      var onFail = function(errors) {
+        _this.set('registrationFailed', true);
+      };
+
+      user.save().then(onSuccess,onFail);
     }
   }
 });
