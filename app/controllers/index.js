d5fc630d6af9843fff5aaac50ad3746aa3ee5484
Monty Lennie <montylennie@gmail.com>
2014-12-20 13:43:02 +0100

    get pre subscription working

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  //properties
   mailFailed: false,
   mailSuccessful: false,
   subscribeFailed: false,
@@ -11,9 +12,16 @@ export default Ember.Controller.extend({
   contactEmailEmail: '',
   contactEmailName: '',
   contactEmailContent:'',
+
+  //computed
   contactEmail: function() {
     return this.store.createRecord('contact-email', {});
   }.property(),
+  preSubscription: function() {
+    return this.store.createRecord('pre-subscriber', {});
+  }.property(),
+
+  //actions
   actions: {
     sendContactEmail: function() {
       //set contact email
@@ -33,27 +41,27 @@ export default Ember.Controller.extend({
         _this.set('mailFailed', true);
       };
 
-      ce.save().then(onSuccess,onFail);
+      ce.save().then(onMailSuccess,onMailFail);
     },
     preSubscribe: function() {
-      //set contact email
-      var ps = this.get('preSubscriber');
-      ps.set('first_name', this.get('subscribeFirstName'));
-      ps.set('last_name', this.get('subscribeLastName'));
+      //set pre subscription
+      var ps = this.get('preSubscription');
+      ps.set('firstName', this.get('subscribeFirstName'));
+      ps.set('lastName', this.get('subscribeLastName'));
       ps.set('email', this.get('subscribeEmail'));
 
-      //save contact email
+      //save presubscription 
       var _this = this;
       var onSubscribeSuccess = function(){
         _this.set('subscribeFailed', false);
-        _this.set('SubscribeSuccessful', true);
+        _this.set('subscribeSuccessful', true);
       };
 
       var onSubscribeFail = function() {
         _this.set('subscribeFailed', true);
       };
 
-      ce.save().then(onSuccess,onFail);
+      ps.save().then(onSubscribeSuccess,onSubscribeFail);
     }
   }
 });
