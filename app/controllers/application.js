cb83a668aa8d6990f57b720f26f1291010f9a411
Monty Lennie <montylennie@gmail.com>
2014-12-21 15:27:49 +0100

    add select fields

---
+++ app/controllers/application.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	restaurants: ['Restaurant, lieu','blue nile', 'fouquets'],
+	cuisines: ['Type de cuisine', 'French', 'Italian', 'World']
+});
