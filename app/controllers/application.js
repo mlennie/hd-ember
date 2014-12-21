c49618e0c51f570e81718fa4ff6cab65c2108fe7
Monty Lennie <montylennie@gmail.com>
2014-12-21 17:34:22 +0100

    setup routing for results page

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,5 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	restaurants: ['Restaurant, lieu','blue nile', 'fouquets'],
-	cuisines: ['Type de cuisine', 'French', 'Italian', 'World']
+	cuisines: ['Type de cuisine', 'French', 'Italian', 'World'],
+	date: '',
+	hours: ['Heure','1h','2h'],
+	couverts: ['Nombre de couverts', 1, 2, 3, 4, 5, 6, 7, 8, 9]
 });
--- app/router.js
+++ app/router.js
@@ -9,6 +9,12 @@ Router.map(function() {
   this.route('protected');
   this.route('login', { path: '/login/:confirmation' });
   this.route('register');
+  this.route('results');
+  this.resource('users', function() {
+  	this.resource('user', { path: '/:user_id'}, function() {
+  	  this.route('results');
+  	});
+  });
 });
 
 export default Router;
