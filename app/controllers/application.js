5400124412fd0e9981381cbf3e7afb02e364ca1f
Monty Lennie <montylennie@gmail.com>
2014-12-21 20:27:12 +0100

    add restaurant list to results page

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	name: '',
 	names: function() {
 		return this.get('model').getEach('name');
 	}.property(),
 	cuisines: ['Type de cuisine', 'French', 'Italian', 'World'],
 	date: '',
 	hours: ['Heure','1h','2h'],
-	couverts: ['Nombre de couverts', 1, 2, 3, 4, 5, 6, 7, 8, 9]
+	couverts: ['Nombre de couverts', 1, 2, 3, 4, 5, 6, 7, 8, 9],
+	actions: {
+		search: function() {
+			this.transitionTo('results', {queryParams: {name: this.get('name')}});
+		}
+	}
 });
