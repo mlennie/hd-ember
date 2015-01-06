e06f83eed4d2035c5548ee08f88bc499b9ceb437
Monty Lennie <montylennie@gmail.com>
2015-01-06 22:22:33 +0100

    add password reset page

--- app/router.js
+++ app/router.js
@@ -10,8 +10,8 @@ Router.map(function() {
     this.route('new');
     this.route('edit', { path: ':user_id/edit' });
     this.route('show', { path: ':user_id' });
+    this.route('reset-password');
   });
-  this.route('protected');
   this.route('login', { path: '/login/:confirmation' });
   this.route('register');
   this.route('results');
