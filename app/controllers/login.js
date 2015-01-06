4207a14371768a839bd1a8bebf97a3c27ccb301f
Monty Lennie <montylennie@gmail.com>
2015-01-06 13:19:58 +0100

    add sign in error message

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,12 +2,38 @@ import Ember from 'ember';
 import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
 export default Ember.Controller.extend(
+
+	//authentication mixin
   LoginControllerMixin, {
+
+  //properties
   authenticator: 'simple-auth-authenticator:devise',
+  identification: null,
+  password: null,
+  loginError: false,
+  isSubmitting: false,
+
+  //computed
   confirmation_success: function() {
     return this.get('params.confirmation') === 'confirmation_success';
   }.property(),
   confirmation_fail: function() {
     return this.get('params.confirmation') === 'confirmation_fail';
-  }.property()
+  }.property(),
+
+  //actions
+  actions: {
+    authenticate: function() {
+      var data = this.getProperties('identification', 'password');
+
+      this._super(data);
+
+      this.set('password', null);
+
+      this.setProperties({
+        loginError: true,
+        loginResponse: 'login error'
+      });
+    }
+  }
 });
--- app/controllers/results.js
+++ app/controllers/results.js
@@ -7,7 +7,7 @@ export default Ember.Controller.extend({
     var name = this.get('name');
     var restaurants = this.get('model');
 
-    if (name == '75017' || name == '75008') {
+    if (name === '75017' || name === '75008') {
       return restaurants.filterBy('zipcode', name);
     } else {
       return restaurants.filterBy('name', name);
@@ -17,7 +17,7 @@ export default Ember.Controller.extend({
     var name = this.get('name');
     var restaurants = this.get('model');
 
-    if (name == '75017' || name == '75008') {
+    if (name === '75017' || name === '75008') {
       return restaurants.rejectBy('zipcode', name);
     } else {
       return restaurants.rejectBy('name', name);
