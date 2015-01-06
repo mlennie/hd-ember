bd3454303fa54760d8412e49a9c8d1201fcc56a9
Monty Lennie <montylennie@gmail.com>
2015-01-06 18:48:56 +0100

    add user scaffold and update edit page to use emblem

--- app/router.js
+++ app/router.js
@@ -6,15 +6,15 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('users', function() {
+    this.route('new');
+    this.route('edit', { path: ':user_id/edit' });
+    this.route('show', { path: ':user_id' });
+  });
   this.route('protected');
   this.route('login', { path: '/login/:confirmation' });
   this.route('register');
   this.route('results');
-  this.resource('users', function() {
-  	this.resource('user', { path: '/:user_id'}, function() {
-  	  this.route('results');
-  	});
-  });
 });
 
 export default Router;
