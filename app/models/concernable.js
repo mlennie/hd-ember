fe023433a15ae6152d871262a1a4ffd6c4b52ffe
Monty Lennie <montylennie@gmail.com>
2015-01-08 22:26:49 +0100

    take out associations to try and fix session bug

--- app/models/concernable.js
+++ app/models/concernable.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  wallet: DS.belongsTo('wallet')
+  //wallet: DS.belongsTo('wallet')
 });
--- app/models/wallet.js
+++ app/models/wallet.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   balance: DS.attr('number'),
-  concernable: DS.belongsTo('concernable', { polymorphic: true, async: true })
+  //concernable: DS.belongsTo('concernable', { polymorphic: true, async: true })
 });
