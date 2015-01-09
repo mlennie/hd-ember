e38a05b5936bdfbe3f80e2727c2890406ae84f83
Monty Lennie <montylennie@gmail.com>
2015-01-09 18:15:35 +0100

    reset user edit message on page change

--- app/routes/users/edit.js
+++ app/routes/users/edit.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	setupController: function(controller, model) {
+    controller.set('success', false);
+    controller.set('fail', false);
+    this._super(controller, model);
+  }
 });
