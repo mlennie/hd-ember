dfc9e5884cad4e2ee16e1abbd775d767c7527dd0
Monty Lennie <montylennie@gmail.com>
2015-01-09 16:19:15 +0100

    add restaurant address to restaurant list

--- app/controllers/restaurants/show.js
+++ app/controllers/restaurants/show.js
@@ -8,12 +8,6 @@ export default Ember.Controller.extend({
   backgroundUrl: function() {
     return 'background-image: url(' + this.get('model.imgUrl')+');';
   }.property('model'),
-  fullAddress: function() {
-    var street = this.get('model.street');
-    var city = this.get('model.city');
-    var zipcode = this.get('model.zipcode');
-    return street + ', ' + city + ', ' + zipcode;
-  }.property('model'),
 
   //actions
   actions: {
--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -6,5 +6,11 @@ export default DS.Model.extend({
   description: DS.attr('string'),
   zipcode: DS.attr('string'),
   street: DS.attr('string'),
-  city: DS.attr('string')
+  city: DS.attr('string'),
+  fullAddress: function() {
+    var street = this.get('street');
+    var city = this.get('city');
+    var zipcode = this.get('zipcode');
+    return street + ', ' + city + ', ' + zipcode;
+  }.property('zipcode','street','city')
 });
