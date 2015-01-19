b99885ddf4dd7d2d79609b902a33235b151f8aae
Monty Lennie <montylennie@gmail.com>
2015-01-19 18:07:01 +0100

    add cookies warning plugin and refactor

--- app/routes/register.js
+++ app/routes/register.js
@@ -3,11 +3,13 @@ import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
 	setupController: function(controller, model) {
-    controller.set('registrationSuccessful', false);
-    controller.set('registrationFailed', false);
-    controller.set('genderBlank', false);
-    controller.set('passwordMismatch', false);
-    controller.set('passwordTooShort', false);
+    controller.setProperties({
+    	registrationSuccessful: false,
+    	registrationFailed: false,
+    	genderBlank: false,
+    	passwordMismatch: false,
+    	passwordTooShort: false
+    });
     this._super(controller, model);
   }
 });
