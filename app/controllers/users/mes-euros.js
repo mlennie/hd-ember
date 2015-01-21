bb7890ced90baba7fe14f02989e61ff776a7f794
Monty Lennie <montylennie@gmail.com>
2015-01-21 11:38:57 +0100

    refactor users controllers code into mixin

--- app/controllers/users/mes-euros.js
+++ app/controllers/users/mes-euros.js
@@ -1,30 +1,6 @@
 import Ember from 'ember';
-export default Ember.Controller.extend({
+import CurrentUserMixin from '../../mixins/current-user';
 
-	//computed properties
-
-  //user
-  currentUser: function() {
-    return this.get('session.currentUser');
-  }.property('session.currentUser'),
-	email: function(){
-		return this.get('currentUser.email');
-	}.property('currentUser.email'),
-	firstName: function(){
-		return this.get('currentUser.firstName');
-	}.property('currentUser.email'),
-	lastName: function(){
-		return this.get('currentUser.lastName');
-	}.property('currentUser.lastName'),
-
-  //wallet
-  wallet: function() {
-    return this.get('currentUser.wallet');
-  }.property('currentUser.wallet'),
-  balance: function() {
-    return this.get('wallet.balance');
-  }.property('wallet.balance'),
-  hasBalance: function() {
-    return this.get('balance') != null;
-  }.property('balance')
+export default Ember.Controller.extend(CurrentUserMixin, {
+  
 });
---
+++ app/mixins/current-user.js
@@ -0,0 +1,34 @@
+import Ember from "ember";
+export default Ember.Mixin.create({
+	//computed properties
+  currentUser: function() {
+    return this.get('session.currentUser');
+  }.property('session.currentUser'),
+	email: function(){
+		return this.get('currentUser.email');
+	}.property('currentUser.email'),
+	firstName: function(){
+		return this.get('currentUser.firstName');
+	}.property('currentUser.email'),
+	lastName: function(){
+		return this.get('currentUser.lastName');
+	}.property('currentUser.lastName'),
+	isOwner: function() {
+		return this.get('currentUser.isOwner');
+	}.property('currentUser.isOwner'),
+	restaurant: function() {
+		var restaurants = this.get('currentUser.restaurants');
+		return restaurants.get('firstObject');
+	}.property('currentUser.restaurants'),
+
+	//wallet
+  wallet: function() {
+    return this.get('currentUser.wallet');
+  }.property('currentUser.wallet'),
+  balance: function() {
+    return this.get('wallet.balance');
+  }.property('wallet.balance'),
+  hasBalance: function() {
+    return this.get('balance') != null;
+  }.property('balance')
+});
