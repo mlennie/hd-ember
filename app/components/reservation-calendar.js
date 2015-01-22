2463bff0cd75a0620735b1ed38725f2df90edfff
Monty Lennie <montylennie@gmail.com>
2015-01-22 16:14:27 +0100

    get calendar working and update day colors based on data-date attributes

--- app/components/reservation-calendar.js
+++ app/components/reservation-calendar.js
@@ -2,7 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	initializeCalendar: function() {
-      return Ember.$("#calendar").fullCalendar(
-      );
-    }.on("didInsertElement")
+		var date = new Date();
+		var d = date.getDate();
+		var m = date.getMonth();
+		var y = date.getFullYear();
+    Ember.$("#calendar").fullCalendar({
+    	theme: false,
+    		events: [
+					{
+						title: 'All Day Event',
+						start: new Date(y, m, 23),
+						allDay: true
+					},
+					{
+						title: 'All Day Event',
+						start: new Date(y, m, 24),
+						allDay: true
+					}
+				]
+    });
+    Ember.$(".fc-past").css({"background-color": "#EEE", "border": "solid 1px #999"});
+    Ember.$(".fc-future[data-date='2015-01-24'], .fc-today[data-date='2015-01-22']").css({"background-color": "yellow", "border": "solid 1px #999"});
+    Ember.$(".fc-future, .fc-today").css({"border": "solid 1px #999"});
+  }.on("didInsertElement")
 });
