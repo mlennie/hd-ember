1d47614bf621a33a809c4f3362b4856ad43cded1
Monty Lennie <montylennie@gmail.com>
2015-01-22 18:45:50 +0100

    highlight current day in calendar and add click event

--- app/components/reservation-calendar.js
+++ app/components/reservation-calendar.js
@@ -7,22 +7,29 @@ export default Ember.Component.extend({
 		var m = date.getMonth();
 		var y = date.getFullYear();
     Ember.$("#calendar").fullCalendar({
-    	theme: false,
-    		events: [
-					{
-						title: 'All Day Event',
-						start: new Date(y, m, 23),
-						allDay: true
-					},
-					{
-						title: 'All Day Event',
-						start: new Date(y, m, 24),
-						allDay: true
-					}
-				]
+    	height: 300,
+    	fixedWeekCount: false,
+			dayRender: function(date, cell) {
+				var y = date.year();
+				var m = date.month();
+				var d = date.date();
+        if (d === moment().date()) {
+        	cell.prepend("<a href='#'></a>");
+        	cell.html("<p id='calendar-percent'>-10%</p>");
+        	cell.css('background-color', 'yellow');
+    		}
+			},
+			dayClick: function(date, jsEvent, view) {
+
+    	}
     });
-    Ember.$(".fc-past").css({"background-color": "#EEE", "border": "solid 1px #999"});
-    Ember.$(".fc-future[data-date='2015-01-24'], .fc-today[data-date='2015-01-22']").css({"background-color": "yellow", "border": "solid 1px #999"});
-    Ember.$(".fc-future, .fc-today").css({"border": "solid 1px #999"});
+    //alert(new Date().getDate());
+    //Ember.$(".fc-past").css({"background-color": "#eee"});
+    //Ember.$(".fc-future[data-date='2015-01-24'], .fc-today[data-date='2015-01-22']").css({"background-color": "yellow"});
+    //Ember.$(".fc-future[data-date='2015-01-24'], .fc-today[data-date='2015-01-22']").text("future");
+    Ember.$(".fc-day").css({"border": "solid 1px #999"});
   }.on("didInsertElement")
 });
+
+//Sun Dec 28 2014 00:00:00 GMT+0000
+//Thu Jan 22 2015 16:58:23 GMT+0100 (CET)
