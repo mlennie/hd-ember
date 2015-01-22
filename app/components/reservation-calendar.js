d6495afd000dd27fa7742ab15daa001596fa0e9c
Monty Lennie <montylennie@gmail.com>
2015-01-22 19:07:03 +0100

    make past days grey

--- app/components/reservation-calendar.js
+++ app/components/reservation-calendar.js
@@ -1,14 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
 	initializeCalendar: function() {
+
 		var date = new Date();
 		var d = date.getDate();
 		var m = date.getMonth();
 		var y = date.getFullYear();
+
     Ember.$("#calendar").fullCalendar({
+
     	height: 300,
     	fixedWeekCount: false,
+
 			dayRender: function(date, cell) {
 				var y = date.year();
 				var m = date.month();
@@ -19,17 +24,16 @@ export default Ember.Component.extend({
         	cell.css('background-color', 'yellow');
     		}
 			},
-			dayClick: function(date, jsEvent, view) {
 
+			dayClick: function(date, jsEvent, view) {
+				alert(date.format());
     	}
     });
+
     //alert(new Date().getDate());
-    //Ember.$(".fc-past").css({"background-color": "#eee"});
+    //color all background colors for past days disabled
+    Ember.$(".fc-past").css({"background-color": "#DDD"});
     //Ember.$(".fc-future[data-date='2015-01-24'], .fc-today[data-date='2015-01-22']").css({"background-color": "yellow"});
     //Ember.$(".fc-future[data-date='2015-01-24'], .fc-today[data-date='2015-01-22']").text("future");
-    Ember.$(".fc-day").css({"border": "solid 1px #999"});
   }.on("didInsertElement")
 });
-
-//Sun Dec 28 2014 00:00:00 GMT+0000
-//Thu Jan 22 2015 16:58:23 GMT+0100 (CET)
