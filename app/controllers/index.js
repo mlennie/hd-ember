2ccdad99ff46e975b7098c3226a04e30f35730f0
Monty Lennie <montylennie@gmail.com>
2015-01-26 16:27:46 +0100

    fix array bug to get restaurant id and transition search to restaurant page when specific restaurant is selected

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -14,6 +14,7 @@ export default Ember.Controller.extend(SearchMixin,{
     return this.store.createRecord('contact-email', {});
   }.property(),
 
+
   //actions
   actions: {
     sendContactEmail: function() {
--- app/mixins/search.js
+++ app/mixins/search.js
@@ -1,6 +1,6 @@
 import Ember from "ember";
 export default Ember.Mixin.create({
-	
+
 	//properties
 	//set initial search options to be null
 	name: null,
@@ -44,12 +44,12 @@ export default Ember.Mixin.create({
 	}.property('model'),
 
 	//setup hours to be shown for select box
-	hours: ['Heure','13:00','13:00','14:00','14:30','15:00','15:30','16:00',
+	hours: ['13:00','13:00','14:00','14:30','15:00','15:30','16:00',
 					'16:30','17:00','17:30','18:00','18:30','19:00','19:30','20:00',
 					'20:30','21:00','21:30','22:00','22:30','23:30'],
 
 	//setup number of people to be shown for select box
-	couverts: ['Nombre de couverts', 1, 2, 3, 4, 5, 6, 7, 8, 9],
+	couverts: [1, 2, 3, 4, 5, 6, 7, 8, 9],
 
 	//actions
 	actions: {
@@ -57,8 +57,45 @@ export default Ember.Mixin.create({
 		//when search button clicked go to search results page
 		//with query params that were selected
 		search: function() {
+			//reset page location to top of page
     	window.scrollTo(0, 0);
-			this.transitionToRoute('restaurants.search-results', {queryParams: {name: this.get('name'), cuisine: this.get('cuisine')}});
+    	//get name of restaurant
+			var name = this.get('name');
+    	//go to search results page and list restaurants that match criteria 
+    	//if specific restaurant has not been picked
+    	if (
+				name === '75017' ||
+				name === '75008' ||
+				name === null
+			) {
+				this.transitionToRoute(
+					'restaurants.search-results', 
+					{ queryParams: { 
+						name: this.get('name'), 
+						cuisine: this.get('cuisine'),
+						date: this.get('date'),
+						time: this.get('time'),
+						number: this.get('number')
+					}}
+				);
+			//else go to the specific restaurant page that was picked
+			//and send chosen query params so that can look up service times with that
+			} else {
+				//get restaurant id by name 
+				var filteredRestaurants = this.get('model').filterBy('name', name);
+    		var restaurantId = filteredRestaurants.get('firstObject').get('id');
+				//transition to restaurant page
+				this.transitionToRoute(
+					'restaurants.show',
+					restaurantId,
+					{ queryParams: { 
+						cuisine: this.get('cuisine'),
+						date: this.get('date'),
+						time: this.get('time'),
+						number: this.get('number')
+					}}
+				);
+			}
 		}
 	}
 });
