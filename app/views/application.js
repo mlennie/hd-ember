68d0e5db73faec1fdc9a7b2e8addd80a165394a0
Monty Lennie <montylennie@gmail.com>
2015-01-28 18:25:44 +0100

    collapse mobile navber when item from dropdown is clicked

--- app/views/application.js
+++ app/views/application.js
@@ -6,5 +6,12 @@ export default Ember.View.extend({
       Ember.$(this).popover();
       Ember.$(this).popover('show');
    	});
-	}.observes('controller.filteredRestaurants').on('didInsertElement')
+	}.observes('controller.filteredRestaurants').on('didInsertElement'),
+
+	//close collapsable navbar dropdown when 
+	closeNavbar: function() {
+		Ember.$('.navbar-collapse .dropdown-menu').click(function(){
+    	Ember.$(".navbar-collapse").collapse('hide');
+		});
+	}.on('didInsertElement'),
 });
