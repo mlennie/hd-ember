521d31d732754ca695fffe173c21d95a598c2904
Monty Lennie <montylennie@gmail.com>
2015-01-28 12:22:52 +0100

    update hours format in search bar

--- app/mixins/search.js
+++ app/mixins/search.js
@@ -8,9 +8,10 @@ export default Ember.Mixin.create({
 	}.property(),
 	cuisines: ['Type de cuisine', 'French', 'Italian', 'World'],
 	date: '',
-	hours: ['Heure','13h','13h30','14h','14h30','15h','15h30','16h','16h30','17h',
-					'17h30','18h','18h30','19h','19h30','20h','20h30','21h','21h30','22h', 
-					'22h30','23h'],
+	//setup hours to be shown for select box
+	hours: ['13:00','13:00','14:00','14:30','15:00','15:30','16:00',
+					'16:30','17:00','17:30','18:00','18:30','19:00','19:30','20:00',
+					'20:30','21:00','21:30','22:00','22:30','23:30'],
 	couverts: ['Nombre de couverts', 1, 2, 3, 4, 5, 6, 7, 8, 9],
 	actions: {
 		search: function() {
