7b6427a0a732163c96639e8e7db46b581292a0f3
Monty Lennie <montylennie@gmail.com>
2015-01-29 14:05:28 +0100

    take out cookie plugin and put in custom cookie header on bottom of page that, when closed adds a cookies accepted header

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,7 +2,26 @@ import Ember from 'ember';
 import SearchMixin from '../mixins/search';
 
 export default Ember.Controller.extend(SearchMixin,{
+	
+	//computed properties
+	showCookieMessage: function() {
+		//get cookieAccepted cookie and if not there (cookies not accepted)
+		//then show cookie message
+    var cookie = this.get('cookie');
+    var cookiesAccepted = cookie.getCookie('cookiesAccepted');
+    return (cookiesAccepted === undefined) ? true : false ;
+	}.property('currentPath'),
+
 	currentPathChanged: function () {
     window.scrollTo(0, 0);
-  }.observes('currentPath')
+  }.observes('currentPath'),
+
+  //actions
+  actions: {
+  	closeAndAcceptCookies: function() {
+  		var cookie = this.get('cookie');
+	    cookie.setCookie('cookiesAccepted', true);
+	    this.set('showCookieMessage', false);
+  	}
+  }
 });
