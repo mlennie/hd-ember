ec6924ce272b598c99dd9aab51b7e0bd3dcbb968
Monty Lennie <montylennie@gmail.com>
2015-02-04 17:29:09 +0100

    make footer and non logged in home page for mobile responsive

--- app/views/index.js
+++ app/views/index.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
+	classNames: ['special-height'],
+	
 	formatSearchFields: function(){
    	Ember.$("#date").mask("99/99/99",{placeholder:"mm/dd/yy"});
 	}.on('didInsertElement'),
