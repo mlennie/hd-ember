242bfd8b7aa5abdc446e8f271db90e73b0168a83
Monty Lennie <montylennie@gmail.com>
2015-02-06 21:30:37 +0100

    make restaurant list clickable and add phone popup

--- app/views/restaurants/search-results.js
+++ app/views/restaurants/search-results.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
 	jqueryEvents: function(){
-    Ember.$('[data-toggle="popover"]').popover();
+    Ember.$('[data-toggle="popover"]').popover().click(function( event ) {
+  		event.preventDefault();
+  		event.stopPropagation();
+  	});
   }.observes('controller.filteredRestaurants').on('didInsertElement')
 });
