cf3fceed73946837fb57579ac031c3c13e9f595a
Monty Lennie <montylennie@gmail.com>
2015-02-08 17:55:42 +0100

    add menu with menu items to restaurant show page

--- app/controllers/restaurants/show.js
+++ app/controllers/restaurants/show.js
@@ -5,6 +5,34 @@ export default Ember.Controller.extend({
   showReserveButton: true,
 
 	//computed properties
+  //get first menu for restaurant
+  firstMenu: function() {
+    return this.get('model.menus').get('firstObject');
+  }.property('model'),
+  menuItems: function() {
+    return this.get('firstMenu.menuItems');
+  }.property('firstMenu'),
+
+  //get orderve menu items
+  orderveMenuItems: function() {
+    return this.get('menuItems').filterBy('course', 'Hors_doeuvre');
+  }.property('firstMenu'),
+
+  //get entree menu items
+  entreeMenuItems: function() {
+    return this.get('menuItems').filterBy('course', 'entree');
+  }.property('firstMenu'),
+
+  //get principaux menu items
+  principauxMenuItems: function() {
+    return this.get('menuItems').filterBy('course', 'principaux');
+  }.property('firstMenu'),
+
+  //get boisson menu items
+  boissonMenuItems: function() {
+    return this.get('menuItems').filterBy('course', 'boisson');
+  }.property('firstMenu'),
+
   backgroundUrl: function() {
     return 'background-image: url(' + this.get('model.imgUrl')+');';
   }.property('model'),
