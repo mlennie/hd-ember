ad69ecccd8b49350f4c18a37764a21d6c135c4c6
Monty Lennie <montylennie@gmail.com>
2015-02-12 19:15:25 +0100

    add loading spinner and shuffle restaurants when rechercher is clicked on search results page

--- app/views/restaurants/search-results.js
+++ app/views/restaurants/search-results.js
@@ -10,7 +10,10 @@ export default Ember.View.extend({
   
   resetRestaurantSorting: function(){
   	var controller = this.controller;
-    Ember.$('button').click(function() {
+    Ember.$('button#top-search-button').click(function() {
+    	Ember.$('.load-spinner').css('display', 'block');
+	    setTimeout(function(){
+	      Ember.$('.load-spinner').css('display', 'none');}, 500);
     	controller.send('changeSortBy');
     });
   }.observes('controller.currentPath').on('didInsertElement')
