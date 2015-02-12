05102884abc329285fda8d78f7a8221472a5e4bb
Monty Lennie <montylennie@gmail.com>
2015-02-12 16:16:49 +0100

    fix search times and search page wording

--- app/mixins/search.js
+++ app/mixins/search.js
@@ -9,9 +9,9 @@ export default Ember.Mixin.create({
 	cuisines: ['Type de cuisine', 'French', 'Italian', 'World'],
 	date: '',
 	//setup hours to be shown for select box
-	hours: ['13:00','13:00','14:00','14:30','15:00','15:30','16:00',
+	hours: ['13:00','13:30','14:00','14:30','15:00','15:30','16:00',
 					'16:30','17:00','17:30','18:00','18:30','19:00','19:30','20:00',
-					'20:30','21:00','21:30','22:00','22:30','23:30'],
+					'20:30','21:00','21:30','22:00','22:30','23:00','23:30'],
 	couverts: ['Nombre de couverts', 1, 2, 3, 4, 5, 6, 7, 8, 9],
 	actions: {
 		search: function() {
