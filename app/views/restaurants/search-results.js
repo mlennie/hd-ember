c62a133690800699cc380ee4de94436f48d35613
Monty Lennie <montylennie@gmail.com>
2015-02-13 18:15:12 +0100

    fix reserver popup in firefox

--- app/views/restaurants/search-results.js
+++ app/views/restaurants/search-results.js
@@ -1,11 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-	jqueryEvents: function(){
-    Ember.$('[data-toggle="popover"]').popover().click(function( event ) {
-  		event.preventDefault();
-  		event.stopPropagation();
-  	});
+	handlePopovers: function(){
+
+    //close popovers when click anywhere
+    Ember.$('body').on('click', function() {
+      Ember.$('*[data-toggle="popover"]').popover();
+      Ember.$('*[data-toggle="popover"]').popover('hide');
+    });
+
+    //open popover and close other popovers
+    Ember.$('body').on('click', '*[data-toggle="popover"]', function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      Ember.$('*[data-toggle="popover"]').not(this).popover('hide');
+      Ember.$(this).popover();
+      Ember.$(this).popover('show');
+      Ember.$(this).popover().css('z-index', -5);
+    });
   }.observes('controller.filteredRestaurants').on('didInsertElement'),
   
   //shuffle restaurants and add loading spinner when rechercher is clicked
