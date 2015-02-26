cdbdaf7b37ace7ee8fac9f1a40a4f5af107bd829
Monty Lennie <montylennie@gmail.com>
2015-02-26 13:20:05 +0100

    fix facebook referral bug

--- app/controllers/users/parrainer-un-ami.js
+++ app/controllers/users/parrainer-un-ami.js
@@ -20,7 +20,8 @@ export default Ember.Controller.extend(CurrentUserMixin, {
 	//get query string referral url
 	queryReferralUrl: function() {
 		var url = "https%3A%2F%2Fwww.happydining.fr%2Fregister%3FreferralCode%3D"
-		return url + this.get('currentUser.referralCode');
+		//must add referral to querry string. If not facebook does not work sometimes
+		return url + 'referral' + this.get('currentUser.referralCode');
 	}.property('referralCodeUrl'),
 
 	//get info for referall email link
@@ -36,7 +37,7 @@ export default Ember.Controller.extend(CurrentUserMixin, {
 	facebookShareUrl: function() {
 		var facebookUrl = 'https://www.facebook.com/sharer/sharer.php?';
 		var url = 'u=' + this.get('queryReferralUrl');
-		var title = '&t=Please%20sign%20up%20for%20happy%20dining.%20You%20get%20money%20for%20eating%20out%20and%20if%20you%20sign%20up%20I%20get%20money%20too!'
+		var title = "&t=Si%20tu%20reserves%20un%20restaurant%20tu%20re%C3%A7ois%2010%25%20%C3%A0%2030%25%20du%20montant%20de%20l'addition.%20D'%C3%A9j%C3%A0%205%20euros%20cr%C3%A9dit%C3%A9%20grace%20%C3%A0%20ce%20lien"
 		return facebookUrl + url + title;
 	}.property('referralCodeUrl'),
 
@@ -44,7 +45,7 @@ export default Ember.Controller.extend(CurrentUserMixin, {
 	twitterShareUrl: function() {
 		var twitterUrl = "https://twitter.com/intent/tweet?";
 		var source = "source=https%3A%2F%2Fwww.happydining.fr%2F&";
-		var body = "Please%20sign%20up%20for%20happy%20dining.%20You%20get%20money%20for%20eating%20out%20and%20if%20you%20sign%20up%20I%20get%20money%20too!";
+		var body = "Si%20tu%20reserves%20un%20restaurant%20tu%20re%C3%A7ois%2010%25%20%C3%A0%2030%25%20du%20montant%20de%20l'addition.%20D'%C3%A9j%C3%A0%205%20euros%20cr%C3%A9dit%C3%A9%20grace%20%C3%A0%20ce%20lien";
 		var text = "text=" + body + ":%20" + this.get('queryReferralUrl');
 		return twitterUrl + source + text;
 	}.property('referralCodeUrl')
