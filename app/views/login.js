74bb0108083f2be55269272d967e46f4d69ed154
Monty Lennie <montylennie@gmail.com>
2015-03-04 15:41:58 +0100

    add mix panel events for concept navber link, CGU footer link, and identify user for concept button when logged in and remember me option on login page

--- app/views/login.js
+++ app/views/login.js
@@ -4,5 +4,17 @@ export default Ember.View.extend({
 
   setHtmlBackgroundColor: function() {
   	Ember.$('html').css('background-color', '#ECECEC');
-  }.on('didInsertElement')
+  }.on('didInsertElement'),
+
+  //send events to MIXPANEL
+	loginMixpanelEvents: function() {
+
+		var controller = this.controller;
+
+		//MIXPANEL: Add remember me checkbox select event
+		Ember.$('body').on('click', '#remember-me', function() {
+	    mixpanel.track('Se Souvenir De Moi Select');
+		});
+
+	}.on('didInsertElement')
 });
