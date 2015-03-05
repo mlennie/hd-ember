418f690a913dbbdb7333cdebe145f65ce5f1c810
Monty Lennie <montylennie@gmail.com>
2015-03-05 16:23:45 +0100

    take out alert

--- app/controllers/restaurants/search-item.js
+++ app/controllers/restaurants/search-item.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	backgroundUrl: function() {
-		alert(this.get('imgUrl'));
     return 'background-image: url(' + this.get('imgUrl')+');';
   }.property('imgUrl')
 });
