f0e0bf4f5d80043034bed5e2200a6ca0b56d8720
Monty Lennie <montylennie@gmail.com>
2015-03-12 17:30:11 +0100

    fix phone popup on mes euros page

--- app/views/users/mes-euros.js
+++ app/views/users/mes-euros.js
@@ -4,15 +4,15 @@ export default Ember.View.extend({
 	jqueryEvents: function(){
     //close popovers when click anywhere
     Ember.$('body').on('click', function() {
-      Ember.$('*[data-toggle="popover"]').popover();
-      Ember.$('*[data-toggle="popover"]').popover('hide');
+      Ember.$('*[data-toggle="popover-mes-euros"]').popover();
+      Ember.$('*[data-toggle="popover-mes-euros"]').popover('hide');
     });
 
     //open popover and close other popovers
-    Ember.$('body').on('click', '*[data-toggle="popover"]', function(event) {
+    Ember.$('body').on('click', '*[data-toggle="popover-mes-euros"]', function(event) {
       event.preventDefault();
       event.stopPropagation();
-      Ember.$('*[data-toggle="popover"]').not(this).popover('hide');
+      Ember.$('*[data-toggle="popover-mes-euros"]').not(this).popover('hide');
       Ember.$(this).popover();
       Ember.$(this).popover('show');
       Ember.$(this).popover().css('z-index', -5);
