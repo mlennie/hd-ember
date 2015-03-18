f502e48ff0eedbcba379d6f66d6a4646163ce8de
Monty Lennie <montylennie@gmail.com>
2015-03-18 15:04:59 +0100

    finish reservation history feature

--- app/models/reservation.js
+++ app/models/reservation.js
@@ -4,6 +4,8 @@ export default DS.Model.extend({
   time: DS.attr('date'),
   restaurant_name: DS.attr('string'),
   status: DS.attr('string'),
+  date: DS.attr('string'),
+  earnings: DS.attr('string'),
   user: DS.belongsTo('user', {async: true}),
   restaurant: DS.belongsTo('restaurant', {async: true}),
 });
