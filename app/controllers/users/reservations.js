5bf7c70c9af109c92c715bd111546baf9ac9b754
Monty Lennie <montylennie@gmail.com>
2015-03-26 14:42:10 +0100

    start reservation day components

---
+++ app/controllers/users/reservations.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import CurrentUserMixin from '../../mixins/current-user';
+
+export default Ember.Controller.extend({
+
+days: [1,2,3,4,5,6,7],
+
+//computedProperties
+servicesForWeek: function() {
+	
+}
+
+});
