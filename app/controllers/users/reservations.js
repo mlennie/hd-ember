f295387c1a4013f1295d4c81d0fd6f700f263226
Monty Lennie <montylennie@gmail.com>
2015-03-27 15:22:52 +0100

    get reservation day components working

--- app/controllers/users/reservations.js
+++ app/controllers/users/reservations.js
@@ -1,46 +1,36 @@
 import Ember from 'ember';
 import CurrentUserMixin from '../../mixins/current-user';
 
-export default Ember.Controller.extend({
+export default Ember.ArrayController.extend({
 
 	days: [1,2,3,4,5,6,7],
-	reservationsByDays: [],
+	day1Reservations: null,
+	day2Reservations: null,
+	day3Reservations: null,
+	day4Reservations: null,
+	day5Reservations: null,
+	day6Reservations: null,
+	day7Reservations: null,
 
 	//computed properties
-	reservationsByDayArray: function() {
-		this.send('dayReservations');
-	}.observes('model'),
-
-	actions: {
-		dayReservations: function() {
-			var days = this.get('days');
+	SetReservationsForDays: function() {
+		var days = this.get('days');
 			var reservationsByDay = [];
 
 			for (var i = 0; i < days.length; i++) {
 				var day = days[i];
 				var extra_days = 86400000 * i;
 
-				console.log("---------*****START OF DAY*****--------");
+				
 				var reservations = this.get('model').filter(function(reservation) {
 					var reservation_time = reservation.get('time').setHours(0,0,0,0);
 					var today = new Date();
 					today.setDate(today.getDate() + i);
 					today = today.setHours(0,0,0,0);
-					console.log("START OF FILTER")
-					console.log("reservation time: " + reservation.get('time'));
-					console.log("reservation time midnight: " + reservation_time);
-					console.log("today : " + today);
-					console.log([reservation_time, today]);
-					console.log(reservation_time === today);
-					console.log("END OF FILTER");
 					return reservation_time === today;
 				});
-				reservationsByDay.push([day, reservations]);
-				console.log("---------*****END OF DAY*****----------");
+				this.set('day' + day.toString() + 'Reservations', reservations);
 			}
+	}.observes('model'),
 
-			this.set('reservationsByDays', reservationsByDay);
-			alert(this.get('reservationsByDays'));
-		}
-	}
 });
