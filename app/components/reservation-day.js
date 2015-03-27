d7e008e8f62772e649941e39f411ecaaf74a8267
Monty Lennie <montylennie@gmail.com>
2015-03-27 11:08:45 +0100

    fix reservations component model fetching code

--- app/components/reservation-day.js
+++ app/components/reservation-day.js
@@ -1,15 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
-	//computed properties
-	owner: function() {
-		return this.store.all('user', this.get('session.user_id'));
-	}.property(),
-
-	restaurant: function() {
-		var r = this.store.all('restaurant', {user_id: this.get('owner')});
-		r = r.get('firstObject');
-		return r;
-	}.property()
 });
