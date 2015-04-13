3263533a3a067dc86cb07f6e67b31118f852123b
Monty Lennie <montylennie@gmail.com>
2015-04-13 17:20:42 +0200

    get reservation retrieval working for restaurant owner and parse local storage to see if user is owner or not

--- app/routes/users/mes-euros.js
+++ app/routes/users/mes-euros.js
@@ -2,4 +2,15 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+	model: function() {
+
+		//look at local storage to see if user is restaurant owner and if so 
+		//get reservations for that owner
+		var session = window.localStorage.getItem('ember_simple_auth:session');
+		var parsed_session = JSON.parse(session);
+
+		if (parsed_session["is_owner"]) {
+    	return this.store.find('reservation', { finished: "true" });
+    }
+  }
 });
