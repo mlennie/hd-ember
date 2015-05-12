349f83a208ed0f133ac10f80718b2893352f1904
Monty Lennie <montylennie@gmail.com>
2015-05-12 12:25:20 +0200

    add phone number to front end when reserving

--- app/controllers/restaurants/reservation-confirmation.js
+++ app/controllers/restaurants/reservation-confirmation.js
@@ -9,6 +9,7 @@ export default Ember.Controller.extend({
   time: null,
   number: null,
   name: null,
+  phone: null,
   isLoading: false,
   reservationSuccess: false,
   reservationFail: false,
@@ -28,79 +29,85 @@ export default Ember.Controller.extend({
 
   //actions
   actions: {
-  	confirmReservation: function() {
+  	confirmReservation: function(phone) {
+      debugger;
+      if (!phone) {
+        alert('Veuillez indiquer un numéro');
+      } else {
 
-      //parse date and time
-      var dateArray = this.get('date').split('-');
-      var timeArray = this.get('time').split(':');
-      
-      var year = dateArray[0];
-      var month = dateArray[1] - 1;
-      var day = dateArray[2];
-      var hour = timeArray[0];
-      var minutes = timeArray[1];
+        //parse date and time
+        var dateArray = this.get('date').split('-');
+        var timeArray = this.get('time').split(':');
+        
+        var year = dateArray[0];
+        var month = dateArray[1] - 1;
+        var day = dateArray[2];
+        var hour = timeArray[0];
+        var minutes = timeArray[1];
 
-      var time = new Date(year,month,day,hour,minutes);
+        var time = new Date(year,month,day,hour,minutes);
 
-      //show loading spinner
-      this.set('isLoading', true);
+        //show loading spinner
+        this.set('isLoading', true);
 
-      //build reservation
-  		var reservation = this.store.createRecord('reservation', {
-  			nbPeople: this.get('number'),
-  			time: time,
-  			status: "not_viewed",
-  			restaurant: this.get('model'),
-  			user: this.get('session.currentUser'),
-  			service: null,
-  			discount: this.get('discount') / 100,
-  			userContribution: 0,
-  			bookingName: this.get('name')
-  		});
+        //build reservation
+    		var reservation = this.store.createRecord('reservation', {
+    			nbPeople: this.get('number'),
+    			time: time,
+    			status: "not_viewed",
+    			restaurant: this.get('model'),
+    			user: this.get('session.currentUser'),
+    			service: null,
+    			discount: this.get('discount') / 100,
+    			userContribution: 0,
+    			bookingName: this.get('name'),
+          phone: this.get('phone')
+    		});
 
-      var controller = this;
+        var controller = this;
 
-      //save reservation
-  		reservation.save().then(function(){
-        //hide loading spinner
-        controller.setProperties({
-          isLoading: false,
-          reservationSuccess: true,
-          reservationFail: false,
-          showDiscountError: false,
-          showNoAvailibilitiesError: false
-        });
-      }, function(response) {
-        //reset all properties
-        controller.setProperties({
-          isLoading: false,
-          reservationSuccess: false,
-          reservationFail: false,
-          showDiscountError: false,
-          showNoAvailibilitiesError: false
-        });
-
-        if (response.errors.discount !== null) {
-          //show error message if no availabilites left
-          if (response.errors.discount === 0) {
-            controller.setProperties({
-              discount: response.errors.discount * 100,
-              showNoAvailibilitiesError: true
-            });
-          } else {
-            //show error message if discount different
-            controller.setProperties({
-              discount: response.errors.discount * 100,
-              showDiscountError: true
-            });
-          }
-        } else {
+        //save reservation
+    		reservation.save().then(function(){
           //hide loading spinner
           controller.setProperties({
-            reservationFail: true
+            isLoading: false,
+            reservationSuccess: true,
+            reservationFail: false,
+            showDiscountError: false,
+            showNoAvailibilitiesError: false
           });
-        }
-      });
+        }, function(response) {
+          //reset all properties
+          controller.setProperties({
+            isLoading: false,
+            reservationSuccess: false,
+            reservationFail: false,
+            showDiscountError: false,
+            showNoAvailibilitiesError: false
+          });
+
+          if (response.errors.discount !== null) {
+            //show error message if no availabilites left
+            if (response.errors.discount === 0) {
+              controller.setProperties({
+                discount: response.errors.discount * 100,
+                showNoAvailibilitiesError: true
+              });
+            } else {
+              //show error message if discount different
+              controller.setProperties({
+                discount: response.errors.discount * 100,
+                showDiscountError: true
+              });
+            }
+          } else {
+            //hide loading spinner
+            controller.setProperties({
+              reservationFail: true
+            });
+          }
+        });
+      }
   	}
   }
 });
